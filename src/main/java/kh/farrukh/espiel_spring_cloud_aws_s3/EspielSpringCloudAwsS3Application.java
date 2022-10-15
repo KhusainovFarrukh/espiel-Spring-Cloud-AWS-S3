dc2ec93cb394a313615b444e0e67ce3a7a94450f
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-10-15 23:40:35 +0500

    Initial commit

---
+++ src/main/java/kh/farrukh/espiel_spring_cloud_aws_s3/EspielSpringCloudAwsS3Application.java
@@ -0,0 +1,13 @@
+package kh.farrukh.espiel_spring_cloud_aws_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspielSpringCloudAwsS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspielSpringCloudAwsS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/kh/farrukh/espiel_spring_cloud_aws_s3/EspielSpringCloudAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.farrukh.espiel_spring_cloud_aws_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspielSpringCloudAwsS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
